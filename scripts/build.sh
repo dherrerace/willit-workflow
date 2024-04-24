df930b0d21582d3acad2f1df94960007b8ab11fd
Diego Herrera <dherrera@redhat.com>
2024-04-24 14:35:04 -0400

    Merge results and generate base webpage

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,6 +27,29 @@ fi
 # Build
 cd /opt/src/willit/
 
+if ! test -f willit-config.json
+then
+    echo === Prepare to merge ===
+    mkdir output
+    # Get all specific repo webpages
+    cp -r $(find . -mindepth 2 -maxdepth 2 -type d -iwholename "./output-*") output/
+    # Merge all status-overall.json files into one
+    jq \
+        -s add \
+        $( \
+            echo $( \
+                find . \
+                    -mindepth 2 -maxdepth 2 \
+                    -type f \
+                    -iwholename "./output-*/status-overall.json" \
+            ) \
+        ) > output/status-overall.json
+
+    # Generate landing page
+    echo '{ "repos": [] }' > willit-config.json
+
+fi
+
 python3 willit.py
 
-rsync -azh /opt/src/willit/output/ /opt/output/willit/
+rsync -azh /opt/src/willit/output/ /opt/output/
